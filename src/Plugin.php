a1717820bd3c6b718216f987aa830f214338a266
Joe Huss <detain@interserver.net>
2017-07-30 16:54:52 -0400

    various improvements

--- src/Plugin.php
+++ src/Plugin.php
@@ -12,7 +12,7 @@ use Symfony\Component\EventDispatcher\GenericEvent;
 class Plugin {
 
 	public static $name = 'Googlewallet Plugin';
-	public static $description = 'Allows handling of Googlewallet emails and honeypots';
+	public static $description = 'Allows handling of Googlewallet based Payments through their Payment Processor/Payment System.';
 	public static $help = '';
 	public static $type = 'plugin';
 
