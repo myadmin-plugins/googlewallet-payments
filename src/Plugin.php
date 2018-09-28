e4cfa30424c0d5e67508aca14a207ee1fa1ec1b4
Joe Huss <detain@interserver.net>
2018-09-27 23:19:35 -0400

    updates

--- src/Plugin.php
+++ src/Plugin.php
@@ -54,10 +54,10 @@ class Plugin
 	public static function getRequirements(GenericEvent $event)
 	{
 		$loader = $event->getSubject();
-		$loader->add_requirement('class.Googlewallet', '/../vendor/detain/myadmin-googlewallet-payments/src/Googlewallet.php');
-		$loader->add_requirement('deactivate_kcare', '/../vendor/detain/myadmin-googlewallet-payments/src/abuse.inc.php');
-		$loader->add_requirement('deactivate_abuse', '/../vendor/detain/myadmin-googlewallet-payments/src/abuse.inc.php');
-		$loader->add_requirement('get_abuse_licenses', '/../vendor/detain/myadmin-googlewallet-payments/src/abuse.inc.php');
+		$loader->add_requirement('class.Googlewallet', '/../vendor/detain/googlewallet-payments/src/Googlewallet.php');
+		$loader->add_requirement('deactivate_kcare', '/../vendor/detain/googlewallet-payments/src/abuse.inc.php');
+		$loader->add_requirement('deactivate_abuse', '/../vendor/detain/googlewallet-payments/src/abuse.inc.php');
+		$loader->add_requirement('get_abuse_licenses', '/../vendor/detain/googlewallet-payments/src/abuse.inc.php');
 	}
 
 	/**
